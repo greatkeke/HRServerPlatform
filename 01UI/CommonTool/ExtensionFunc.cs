f086c39d95ed59646d0d73d1572c41facf27bfb6
Feng <kexxxfeng@163.com>
2015-08-23 18:00:15 +0800

    基本完成

---
+++ 01UI/CommonTool/ExtensionFunc.cs
@@ -0,0 +1,55 @@
+﻿using _02BLL;
+using _04Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace _01UI
+{
+    public static class ExtensionFunc
+    {
+        /// <summary>
+        /// 转换为需求展示模型
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<RequirementShowModel> ConvertToRequireShowModel(this IEnumerable<t_Requirement> source)
+        {
+            var cateList = new BaseBLL<t_Categories>().Query(u => true).ToList();
+            List<RequirementShowModel> list = new List<RequirementShowModel>();
+            foreach (var item in source)
+            {
+                list.Add(new RequirementShowModel()
+                {
+                    ID = item.ID,
+                    Title = item.Title,
+                    PostDate = item.PostDate.ToString("yyyy-MM-dd hh:mm"),
+                    CatetogriyName = cateList.Where(u => u.ID == item.ID).Select(u => u.CategoryName).FirstOrDefault() ?? string.Empty,
+                    Gread = ((EnumRequireGread)(item.Gread ?? 0)).ToString(),
+                    Status = ((EnumRequireStauts)(item.Status)).ToString()
+                });
+            }
+            return list;
+        }
+        /// <summary>
+        /// 是否已经包含该页
+        /// </summary>
+        /// <param name="pages"></param>
+        /// <param name="whereStr"></param>
+        /// <returns>如果存在则返回TabPage，否则返回null</returns>
+        public static TabPage WherePage(this TabControl.TabPageCollection pages, string whereStr)
+        {
+            foreach (TabPage item in pages)
+            {
+                if ((item.Tag??string.Empty).ToString() == whereStr)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ 01UI/UC_Info.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _04Model;
+using _02BLL;
+
+namespace _01UI
+{
+    public partial class UC_Info : UserControl
+    {
+        BaseBLL<t_User> userBll = new BaseBLL<t_User>();
+        t_User loginUser;
+        public UC_Info()
+        {
+            InitializeComponent();
+            this.AutoSize = true;
+            this.Load += UC_Info_Load;
+        }
+
+
+        private void UC_Info_Load(object sender, EventArgs e)
+        {
+            if (Program.loginUserID == null)
+            {
+                //return;
+            }
+            //获取当前用户的特长信息
+
+            this.loginUser = userBll.Query(u => u.ID == Program.loginUserID).FirstOrDefault();
+            //展示
+            if (loginUser != null)
+            {
+                this.htmlEditor1.ShowToolBar = false;
+                this.htmlEditor1.ReadOnly = true;
+                this.htmlEditor1.BodyHtml = loginUser.ShowInfo;
+            }
+
+        }
+        /// <summary>
+        /// 编辑
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.htmlEditor1.ShowToolBar = true;
+            this.htmlEditor1.ReadOnly = false;
+        }
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.loginUser.ShowInfo = this.htmlEditor1.BodyHtml??string.Empty;
+            userBll.Update(this.loginUser);
+            MessageBox.Show("保存成功", "提示", MessageBoxButtons.OK);
+        }
+    }
+}
